e85076a5d7de0b5b6d00ee123a6466c875e8a0d4
shayanriyaz <shayanariyaz@gmail.com>
2020-12-01 11:50:02 -0800

    Basic User Auth Working

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { auth } from 'firebase';
+import { useAuth } from '../lib/auth';
 
-export default function Home() {
-  return (
-    <div className={styles.container}>
+const Home = () =>{
+
+  const auth = useAuth();
+  return <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,35 +22,11 @@ export default function Home() {
           <code className={styles.code}>pages/index.js</code>
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {auth?.user ? (
+        <button onClick={(e) => auth.signout()}>Sign Out</button>
+      ): (
+        <button onClick={(e) => auth.signInWithGoogle()}>Sign In</button>
+      )}
       </main>
 
       <footer className={styles.footer}>
@@ -61,5 +40,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
-}
+  };
+  export default Home;
