07fe14a04d726962a6b16de1add79e089856f861
KohinaTheCat <clarachick11@hotmail.com>
2020-12-03 15:58:08 -0500

    started POST route.

--- pages/api/user/createProject/[uid].js
+++ pages/api/user/createProject/[uid].js
@@ -1,11 +1,16 @@
 import db from "@/lib/firebase-admin";
 
 export default (req, res) => {
-  db.collection("project")
-    .doc()
-    .add({title: "test"})
+  db.collection("projects")
+    .add({
+      project: "hi",
+      url: "",
+      createdAt: new Date(),
+      authorId: req.query.uid,
+    })
     .then((doc) => {
       res.json(doc.data());
+      // add to user's list too
     })
     .catch((error) => {
       res.json({ error });
