146e81f7a592f5c7532ed048b032336fe3b3b5e6
KohinaTheCat <clarachick11@hotmail.com>
2020-12-03 20:19:20 -0500

    finished POST createProject route

--- pages/api/user/createProject/[uid].js
+++ pages/api/user/createProject/[uid].js
@@ -1,18 +1,28 @@
 import db from "@/lib/firebase-admin";
 
 export default (req, res) => {
-  db.collection("projects")
-    .add({
-      project: "hi",
-      url: "",
-      createdAt: new Date(),
-      authorId: req.query.uid,
-    })
-    .then((doc) => {
-      res.json(doc.data());
-      // add to user's list too
-    })
-    .catch((error) => {
-      res.json({ error });
-    });
+  return new Promise((resolve, reject) => {
+    db.collection("projects")
+      .add({
+        project: "new newww", // to change
+        url: "",
+        createdAt: new Date(),
+        authorId: req.query.uid,
+      })
+      .then((doc) => {
+        var userRef = db.collection("users").doc(req.query.uid);
+        userRef.get().then((d) => {
+          userRef.update({
+            projectsCreated: d
+              .data()
+              .projectsCreated.concat([doc._path.segments[1]]),
+          });
+        });
+
+        res.status(200).json(doc._path.segments[1]);
+      })
+      .catch((error) => {
+        res.status(400).json({ error });
+      });
+  });
 };
