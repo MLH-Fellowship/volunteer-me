117202e00cca0fef0756d7dbab45f017544186ee
pablomdd <pablo.mdd.007@gmail.com>
2020-12-06 17:02:56 -0600

    add basic google map and styles

--- pages/volunteer.js
+++ pages/volunteer.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { useAuth } from '@/lib/auth';
 import EmptyState from '@/components/EmptyState'
 import DashboardShell from '@/components/DashboardShell';
@@ -6,23 +7,62 @@ import ProjectTable from '@/components/ProjectTable';
 import fetcher from '@/utils/fetcher';
 import useSWR from 'swr';
 
+// G MAP imports
+import {
+    GoogleMap,
+    useLoadScript,
+    Marker,
+    InfoWindow,
+} from "@react-google-maps/api";
+import googleMapStyles from '../components/googleMapStyles';
+
+const mapContainerStyle = {
+    height: "100vh",
+    width: "100vw",
+};
+const mapOptions = {
+    styles: googleMapStyles,
+    disableDefaultUI: true,
+    zoomControl: true,
+};
+const mapDefaultCenter = {
+    //   San Francisco Coords
+    lat: 37.774929,
+    lng: -122.419418,
+};
 
 const Volunteer = () => {
     const auth = useAuth();
-    const { data } = useSWR('/api/projects',fetcher);
+    const { data } = useSWR('/api/projects', fetcher);
 
-    if (!data) {
-        return (
-            <DashboardShell>
-                <ProjectTableSkeleton />
-            </DashboardShell>
-        );
-    }
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    });
+
+    // TODO: Change these returns for skeletons
+    if (loadError) return "Error";
+    if (!isLoaded) return "Loading...";
 
     return (
-        <DashboardShell>
-            {data.projects.length !=0 ? <ProjectTable projects= {data.projects} /> : <EmptyState />}
-        </DashboardShell>
+        <>
+            <Head>
+                <title>Volunteer | Volunteer.me</title>
+            </Head>
+            <DashboardShell>
+                <h1>Volunteer</h1>
+            </DashboardShell>
+            <GoogleMap
+                id="map"
+                mapContainerStyle={mapContainerStyle}
+                zoom={14}
+                center={mapDefaultCenter}
+                options={mapOptions}
+                // onClick={onMapClick}
+                // onLoad={onMapLoad}
+            ></GoogleMap>
+
+        </>
+
     );
 };
 
