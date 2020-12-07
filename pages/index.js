ccdeb8b48e571b598b30126a5f8aebe0856f8fba
pablomdd <pablo.mdd.007@gmail.com>
2020-12-06 20:23:36 -0600

    add @ShayanRiyaz index.js

--- pages/index.js
+++ pages/index.js
@@ -6,56 +6,91 @@ import { useAuth } from '@/lib/auth';
 import { FcGoogle } from 'react-icons/fc';
 
 // 1. import `ChakraProvider` component
-import { ChakraProvider, Button, Icon, Flex, Stack, Heading, Text, Code } from "@chakra-ui/react"
-
-import Link from 'next/link'
+import { Box, Button, Flex, Text, Icon, Link, Stack } from '@chakra-ui/react';
+// import Link from 'next/link'
 
 const Home = () => {
 
   const auth = useAuth();
   return (
-  <Flex
-    as="main"
-    direction="column"
-    align="center"
-    justify="center"
-    h="100vh"
-  >
-    <Head>
-      <title>Volunteer Me</title>
-      {/* <link rel="icon" href="/favicon.ico" /> */}
-    </Head>
+    <Box bg="gray.100">
+      <Flex
+        as="main"
+        direction="column"
+        align="center"
+        justify="center"
+        h="100vh"
+        maxW="400px"
+        margin="0 auto"
+      >
+        <Head>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              if (document.cookie && document.cookie.includes('volunteer-me-auth')) {
+                window.location.href = "/project"
+              }
+            `
+            }}
+          />
+          <title>Volunteer Me</title>
+        </Head>
+        <Icon color="black" name="logo" size="64px" />
+        <Text mb={4} fontSize="lg" p={6}>
+          <Text as="span" fontWeight="bold" display="inline">
+            Volunteer Me
+    </Text>
+          {' is being built as part of '}
+          <Link
+            href="https://fellowship.mlh.io/"
+            isExternal
+            textDecoration="underline"
+          >
+            MLH-Fellowship
+            </Link>
+          {`. It's the easiest way to find volunteers for projects related to social work. It's still a work-in-progress, but you can try it out by logging in.`}
+        </Text>
+        {auth.user ? (
+          <Button
+            as="a"
+            href="/project"
+            backgroundColor="white"
+            color="gray.900"
+            variant="outline"
+            fontWeight="medium"
+            mt={4}
+            size="lg"
+            _hover={{ bg: 'gray.100' }}
+            _active={{
+              bg: 'gray.100',
+              transform: 'scale(0.95)'
+            }}
+          >
 
-
-    <Icon color="black" name="logo" size="64px" />
-
-    {/* <Text>
-        Current user: <Code> {auth.user ? auth.user.email : 'None'}</Code>
-      </Text> */}
-
-    {/* <Stack direction="row" spacing={4}>
-        {auth?.user ? (
-        <Button leftIcon={<GoSignOut />} onClick={(e) => auth.signout()} colorScheme="red" variant="outline">
-          Sign Out
+            View Projects
           </Button>
-      ): (
-        <Button leftIcon={<FcGoogle />} onClick={(e) => auth.signInWithGoogle()} colorScheme="red" variant="solid">
-          Sign In
-          </Button>
-          
-      )}
-      </Stack> */}
-    {auth.user ? (
-      <Button as="a" href="/project">
-        View Dashboard
-      </Button>
-    ) : (
-        <Button leftIcon={<FcGoogle />} mt={4} size="sm" onClick={(e) => auth.signInWithGoogle()}>
-          Sign In
-        </Button>
-      )}
+        ) : (
+            <Button
+              onClick={(e) => auth.signInWithGoogle()}
+              backgroundColor="red"
+              color="gray.900"
+              variant="outline"
+              fontWeight="medium"
+              leftIcon={<FcGoogle />}
+              mt={4}
+              size="lg"
+              _hover={{ bg: 'red.400' }}
+              _active={{
+                bg: 'gray.100',
+                transform: 'scale(0.95)'
+              }}
+            >
+              Sign In with Google
+            </Button>
+          )}
 
-  </Flex>
+      </Flex>
+    </Box>
 
     // <footer className={styles.footer}>
     //   <a
