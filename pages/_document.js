bce0e9dc85b031f91757750e0aa5e298300f09cb
shayanriyaz <shayanariyaz@gmail.com>
2020-12-08 11:28:51 -0800

    updated favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,11 +5,12 @@ class MyDocument extends Document {
         return (
             <Html>
             <Head>
-              <link rel="icon" href="/favicon.ico" />
+              <link rel="icon" href="favicons/favicon.ico" />
               <link
                 href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap"
                 rel="stylesheet"
               />
+
             </Head>
             <body>
               <Main />
