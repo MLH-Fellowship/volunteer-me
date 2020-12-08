42ef2b60fd378b477f64611b430c8bb33dfadbb3
KohinaTheCat <clarachick11@hotmail.com>
2020-12-08 12:27:24 -0500

    fixed api call so it returns a promise. working being called from frontend

--- pages/project/[pid].js
+++ pages/project/[pid].js
@@ -1,10 +1,34 @@
 import React from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
 import { Flex, Skeleton } from "@chakra-ui/react";
+import fetcher from "@/utils/fetcher";
+
 function Proj() {
+  const router = useRouter();
+  const { pid } = router.query;
+
+  if (!pid) {
+    return (<></>)
+  }
+
+  const {
+    authorId,
+    url,
+    name,
+    projectFocus,
+    startDate,
+    endDate,
+    country,
+    city,
+    requiredVolunteers,
+  } = useSWR("/api/project/" + pid, fetcher).data;
+  console.log(useSWR("/api/project/" + pid, fetcher).data);
   return (
     <Flex justify="center">
       <Skeleton w="80vw" h="400px" />
-      owowowowo
+      {pid}
+      {name}
     </Flex>
   );
 }
