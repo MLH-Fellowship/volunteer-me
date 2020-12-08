0594a8868580c476e416b392500f71ba53feaf9a
KohinaTheCat <clarachick11@hotmail.com>
2020-12-08 12:39:21 -0500

    api error fixed again.

--- pages/project/[pid].js
+++ pages/project/[pid].js
@@ -3,32 +3,48 @@ import { useRouter } from "next/router";
 import useSWR from "swr";
 import { Flex, Skeleton } from "@chakra-ui/react";
 import fetcher from "@/utils/fetcher";
+import Layout from "@/components/Layout";
+import DashboardShell from "@/components/DashboardShell";
 
 function Proj() {
   const router = useRouter();
   const { pid } = router.query;
 
   if (!pid) {
-    return (<></>)
+    return <></>;
   }
 
-  const {
-    authorId,
-    url,
-    name,
-    projectFocus,
-    startDate,
-    endDate,
-    country,
-    city,
-    requiredVolunteers,
-  } = useSWR("/api/project/" + pid, fetcher).data;
-  console.log(useSWR("/api/project/" + pid, fetcher).data);
+  var data = useSWR("/api/project/" + pid, fetcher).data;
+  console.log(data);
+
+  if (data) {
+    const {
+      authorId,
+      url,
+      name,
+      projectFocus,
+      startDate,
+      endDate,
+      country,
+      city,
+      requiredVolunteers,
+    } = data;
+
+    console.log(name);
+
+    return (
+      <Layout>
+        {name}
+        <Flex justify="center">
+          <Skeleton w="80vw" h="400px" />
+        </Flex>
+      </Layout>
+    );
+  }
+
   return (
     <Flex justify="center">
       <Skeleton w="80vw" h="400px" />
-      {pid}
-      {name}
     </Flex>
   );
 }
