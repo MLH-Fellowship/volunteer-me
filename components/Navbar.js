33cc477bd0105a5ed25b2c7d865ee0c342d6a6e0
pablomdd <pablo.mdd.007@gmail.com>
2020-12-09 15:34:08 -0600

    secure navbar navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link as NextLink } from "next/link";
 import {
     Button,
     Flex,
@@ -11,7 +12,6 @@ import { GoSignOut } from 'react-icons/go';
 
 function Navbar() {
     const { user, signout } = useAuth();
-    console.log("navbar", user)
     return (
         <Flex backgroundColor="white" mb={16} w="full">
             <Flex
@@ -27,20 +27,29 @@ function Navbar() {
             >
                 <Flex>
                     <Icon name="logo" size="24px" mr={8} />
+                    <Link href="/about">About</Link>
                     {user ?
                         <Link mr={4} href="/project">Project</Link>
                         : null
                     }
                     <Link href="/volunteer" mr={4}>Volunteer</Link>
-                    <Link href="/about">About</Link>
                 </Flex>
                 <Flex justifyContent="center" alignItems="center">
-                    {user && (
-                        <Button leftIcon={<GoSignOut />} variant="ghost" mr={2} onClick={() => signout()}>
-                            Log Out
-                        </Button>
-                    )}
-                    <Avatar size="sm" src={user?.photoUrl} />
+                    {user ? (
+                            <>
+                                <Button leftIcon={<GoSignOut />} variant="ghost" mr={2} onClick={() => signout()}>
+                                    Log Out
+                                </Button>
+                                <Avatar size="sm" src={user?.photoUrl} />
+                            </>
+                        )
+                        :
+                        (
+                            <Link href="/" variant="ghost" mr={2} >
+                                Log In
+                            </Link>
+                        )
+                    }
                 </Flex>
             </Flex>
         </Flex>
